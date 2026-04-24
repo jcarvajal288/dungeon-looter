42f30d201a9cb600c429f2e49bfedc80f7436874
Max Carvajal <jcarvajal288@gmail.com>
2026-04-23 21:13:55 -0500

    Add value and multiplier fields to item_data

--- Items/item_data.gd
+++ Items/item_data.gd
@@ -2,5 +2,7 @@ class_name ItemData extends Resource
 
 @export var name: String = "<Placeholder Item Name>"
 @export var description: String = "<Placeholder Item Description>"
+@export var value: int = 1000
+@export var multiplier: float = 1.0
 @export var small_icon: Texture2D
 @export var large_icon: Texture2D
