d46bceda3aec5a4456dbb83fc9ce3de5499a866e
Max Carvajal <jcarvajal288@gmail.com>
2026-05-05 07:00:50 -0500

    Allow any gem to be put in any plinth

--- Levels/Components/Prop/ItemSlotArea/item_slot_area.gd
+++ Levels/Components/Prop/ItemSlotArea/item_slot_area.gd
@@ -1,23 +1,26 @@
 class_name ItemSlotArea extends InteractionArea
 
-@export var key_item: ItemData
+@export var slotable_item_names: Array[String]
 
-signal key_item_slotted_in
+var item_in_slot = null
+
+signal item_put_in_slot
 
 
 func _ready() -> void:
 	$Sprite2D.visible = false
 
 
+func set_slotable_items(slotable_items: Array[ItemData]) -> void:
+	slotable_item_names.assign(slotable_items.map(func(i): return i.name))
+
+
 func interact_with_item(item: ItemData) -> bool:
-	if item.name == key_item.name:
-		$Sprite2D.texture = key_item.small_icon
+	if item.name in slotable_item_names:
+		item_in_slot = item
+		$Sprite2D.texture = item.small_icon
 		$Sprite2D.visible = true
-		key_item_slotted_in.emit()
+		item_put_in_slot.emit()
 		return true
 	else:
 		return false
-
-
-func is_solved() -> bool:
-	return $Sprite2D.visible
--- Levels/Dungeon/Props/GemPlinth/gem_plinth.gd
+++ Levels/Dungeon/Props/GemPlinth/gem_plinth.gd
@@ -1,15 +1,17 @@
 class_name GemPlinth extends Prop
 
-@export var key_item: ItemData
+@export var correct_item: ItemData
+@export var slotable_items: Array[ItemData]
 
 
 func _ready() -> void:
 	$ItemSlotArea.on_interaction.connect(on_interaction)	
-	$ItemSlotArea.key_item = key_item
+	$ItemSlotArea.set_slotable_items(slotable_items)
 
 
 func on_interaction() -> void:
-	if $ItemSlotArea.is_solved():
-		NotificationScreen.show_message.emit("The %s has been set in the plinth." % key_item.name)
+	var item_in_slot = $ItemSlotArea.item_in_slot
+	if item_in_slot != null:
+		NotificationScreen.show_message.emit("The %s has been set in the plinth." % item_in_slot.name)
 	else:
 		NotificationScreen.show_message.emit("You see a round impression in the plinth.")
