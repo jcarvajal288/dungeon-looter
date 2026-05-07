a7e39d0c9871b481f5df85cf01d52da21c86bb61
Max Carvajal <jcarvajal288@gmail.com>
2026-05-07 07:41:07 -0500

    Disable removing a gem from the solved puzzle

--- Levels/Dungeon/Props/GemPlinth/gem_plinth.gd
+++ Levels/Dungeon/Props/GemPlinth/gem_plinth.gd
@@ -4,6 +4,7 @@ class_name GemPlinth extends Prop
 @export var slotable_items: Array[ItemData]
 
 @onready var is_solved = false
+@onready var is_locked = false
 
 signal solved
 
@@ -18,8 +19,11 @@ func _ready() -> void:
 func on_interaction() -> void:
 	var item_in_slot = $ItemSlotArea.item_in_slot
 	if item_in_slot != null:
-		NotificationScreen.show_message.emit("You take the %s." % item_in_slot.name)
-		$ItemSlotArea.move_item_to_player_inventory()
+		if is_locked:
+			NotificationScreen.show_message.emit("The %s is locked into the plinth." % item_in_slot.name)
+		else:
+			NotificationScreen.show_message.emit("You take the %s." % item_in_slot.name)
+			$ItemSlotArea.move_item_to_player_inventory()
 	else:
 		NotificationScreen.show_message.emit("You see a round impression in the plinth.")
 
@@ -28,3 +32,8 @@ func on_item_put_in_slot(item_name: String) -> void:
 	if item_name == correct_item.name:
 		is_solved = true
 		solved.emit()
+
+
+func correct_gem_in_slot() -> bool:
+	return $ItemSlotArea.item_in_slot != null \
+	and correct_item.name == $ItemSlotArea.item_in_slot.name
--- Levels/Dungeon/Rooms/TestRoom/test_room.gd
+++ Levels/Dungeon/Rooms/TestRoom/test_room.gd
@@ -3,6 +3,9 @@ extends Room
 func _ready() -> void:
 	super()
 	$EasternExit/Door.set_closed_state(PersistentState.door_states["TEST_ROOM_EASTERN_EXIT"])
+	$GemPlinthRed.solved.connect(check_gem_puzzle)
+	$GemPlinthGreen.solved.connect(check_gem_puzzle)
+	$GemPlinthBlue.solved.connect(check_gem_puzzle)
 
 
 func get_entry_point(entry_id: String) -> Vector2:
@@ -15,3 +18,14 @@ func get_entry_point(entry_id: String) -> Vector2:
 	else:
 		printerr("Invalid entry id in TestRoom: ", entry_id)
 		return Vector2.ZERO
+
+
+func check_gem_puzzle() -> void:
+	var red_solved = $GemPlinthRed.correct_gem_in_slot()
+	var green_solved = $GemPlinthGreen.correct_gem_in_slot()
+	var blue_solved = $GemPlinthBlue.correct_gem_in_slot()
+	if red_solved and green_solved and blue_solved:
+		$WesternExit/PuzzleDoor.set_closed_state(false)
+		$GemPlinthRed.is_locked = true
+		$GemPlinthGreen.is_locked = true
+		$GemPlinthBlue.is_locked = true
