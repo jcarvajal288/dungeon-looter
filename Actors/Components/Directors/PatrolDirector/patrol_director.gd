11925e864877a47d611484a9e92e4600bf0a86c1
Max Carvajal <jcarvajal288@gmail.com>
2026-05-12 06:52:18 -0500

    Allow choice between looped and non-looped patrol routs

--- Actors/Components/Directors/PatrolDirector/patrol_director.gd
+++ Actors/Components/Directors/PatrolDirector/patrol_director.gd
@@ -1,7 +1,9 @@
 extends Director
 
+@export var is_loop: bool = true
 var patrol_points: Array[Marker2D]
 var destination_index = 0
+var is_reverse: bool = false
 
 
 func _ready() -> void:
@@ -15,8 +17,21 @@ func _process(_delta: float) -> void:
 	var destination = patrol_points[destination_index]
 	var distance_to_destination = subject.global_position.distance_to(destination.global_position)
 	if distance_to_destination <= 2.0:
-		destination_index += 1
-		if destination_index >= patrol_points.size():
-			destination_index = 0
+		if is_reverse:
+			destination_index -= 1
+			if destination_index < 0:
+				if is_loop:
+					destination_index = patrol_points.size() - 1
+				else:
+					destination_index = 1
+					is_reverse = false
+		else:
+			destination_index += 1
+			if destination_index >= patrol_points.size():
+				if is_loop:
+					destination_index = 0
+				else:
+					destination_index = patrol_points.size() - 1
+					is_reverse = true
 		destination = patrol_points[destination_index]
 	movement_vector = subject.global_position.direction_to(destination.global_position)
