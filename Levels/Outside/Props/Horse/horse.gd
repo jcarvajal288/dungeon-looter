e81f2bdb4e20e4dbf83ebfbabf379f8404de7725
Max Carvajal <jcarvajal288@gmail.com>
2026-05-14 07:49:58 -0500

    Prop cleanup

--- Levels/Outside/Props/Horse/horse.gd
+++ Levels/Outside/Props/Horse/horse.gd
@@ -1,10 +1,5 @@
 class_name Horse extends Prop
 
 
-func _ready() -> void:
-    super()
-    $InteractionArea.on_interaction.connect(on_interaction)
-
-
 func on_interaction() -> void:
     EndScreen.end_game.emit()
--- Levels/Outside/Props/WagonStorageBox/wagon_storage_box.gd
+++ Levels/Outside/Props/WagonStorageBox/wagon_storage_box.gd
@@ -1,9 +1,5 @@
 class_name WagonStorageBox extends Prop
 
-func _ready() -> void:
-    super()
-    $InteractionArea.on_interaction.connect(on_interaction)
-
 
 func on_interaction() -> void:
     StorageBoxScreen.toggle_storage_box_screen.emit()
