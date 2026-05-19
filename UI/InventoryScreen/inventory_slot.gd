eabb851d502673cd3f85d4754a3044819cd1c8b3
Max Carvajal <jcarvajal288@gmail.com>
2026-05-19 07:06:47 -0500

    Only show item count if the item is stackable

--- UI/InventoryScreen/inventory_slot.gd
+++ UI/InventoryScreen/inventory_slot.gd
@@ -5,7 +5,10 @@ func update_slot(item_data: ItemData) -> void:
 	if item_data:
 		$ItemImage.texture = item_data.large_icon
 		$ItemImage.show()
-		$ItemCount.show()
+		if item_data.stackable:
+			$ItemCount.show()
+		else:
+			$ItemCount.hide()
 	else:
 		clear()
 
