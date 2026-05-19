f9c50dfac68b5de69315328f24a9773ac1813948
Max Carvajal <jcarvajal288@gmail.com>
2026-05-19 07:04:59 -0500

    Only show item count if there is an item in the slot

--- UI/InventoryScreen/inventory_slot.gd
+++ UI/InventoryScreen/inventory_slot.gd
@@ -5,6 +5,7 @@ func update_slot(item_data: ItemData) -> void:
 	if item_data:
 		$ItemImage.texture = item_data.large_icon
 		$ItemImage.show()
+		$ItemCount.show()
 	else:
 		clear()
 
@@ -12,3 +13,4 @@ func update_slot(item_data: ItemData) -> void:
 func clear() -> void:
 	$ItemImage.texture = null
 	$ItemImage.hide()
+	$ItemCount.hide()
